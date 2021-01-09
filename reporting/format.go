fd2c09c02d25d157921d8298da8c7b218bb94c56
sebastian <sebastian@numbat>
2021-01-09 13:20:49 +0100

    improved duration rounding/formatting

--- reporting/format.go
+++ reporting/format.go
@@ -8,13 +8,11 @@ import (
 
 func fmtDuration(d time.Duration) string {
 
-	d = d.Round(time.Second)
-
 	h := d / time.Hour
 	d -= h * time.Hour
 	m := d / time.Minute
 	d -= m * time.Minute
-	s := d / time.Second
+	s := d.Round(time.Second) / time.Second
 
 	return fmt.Sprintf("%02dh %02dm %02ds", h, m, s)
 }
